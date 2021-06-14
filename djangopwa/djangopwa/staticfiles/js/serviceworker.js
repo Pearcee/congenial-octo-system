bb779c4b06e2baa02d48782f34c8e15a9c476e97
Steve <sjpearce@gmail.com>
2021-06-14 08:07:25 +0100

    06/14/2021 08:07:25

--- djangopwa/djangopwa/staticfiles/js/serviceworker.js
+++ djangopwa/djangopwa/staticfiles/js/serviceworker.js
@@ -1,10 +1,14 @@
-var staticCacheName = 'djangopwa-v1';
+var staticCacheName = 'djangopwa-v1.2.1';
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        '/base_layout'
+        '/base_layout',
+        "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js",
+        "https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css",
+        "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js",
+        "https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
       ]);
     })
   );
